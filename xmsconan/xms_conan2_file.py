86c605457829c5e8faeb5fb59bfe0c873eb6caff
Gage Larsen <glarsen@aquaveo.com>
2024-11-20 11:06:33 -0700

    initial updates for conan2

---
+++ xmsconan/xms_conan2_file.py
@@ -0,0 +1,225 @@
+"""
+Conanfile base for the xmscore projects compatible with Conan 2.x.
+"""
+import os
+
+import conan
+from conan import ConanFile, tools
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
+from conan.tools.env import Environment
+from conan.tools.files import copy
+from conan.tools.scm import Git
+from conan.errors import ConanException
+
+
+class XmsConan2File(ConanFile):
+    """
+    XmsConan class used for defining the conan info.
+    """
+    license = "FreeBSD Software License"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {
+        "pybind": [True, False],
+        "testing": [True, False],
+    }
+    xms_dependencies = []
+    extra_exports = []
+    extra_export_sources = []
+
+    default_options = {
+        'pybind': False,
+        'testing': False,
+    }
+
+    def set_version(self):
+        """Set the version of the conanfile."""
+        git = Git(self, self.recipe_folder)
+        self.version = git.run("describe --tags")
+
+    # def __init__(self, *args, **kwargs):
+    #     """Initialize things."""
+    #     super(ConanFile, self).__init__(*args, **kwargs)
+    #     env = Environment()
+    #     envvars = env.vars(self)
+    #     self.output.warning(f"env: {env.dumps()}")
+    #     # else:
+    #         # self.generators = ["CMakeDeps", "CMakeToolchain"]
+
+    def requirements(self):
+        """Requirements."""
+        self.requires("boost/1.86.0")
+        self.requires("zlib/1.3.1")
+        if self.options.testing:
+            self.requires('cxxtest/4.4')
+        # Pybind if not clang
+        if not self.settings.compiler == "clang" and self.options.pybind:
+            self.requires("pybind11/2.9.1@aquaveo/stable")
+
+        for dependency in self.xms_dependencies:
+            self.requires(dependency)
+
+    def configure_options(self):
+        """Configure the options for the conan class."""
+        if self.settings.build_type != "Release":
+            del self.options.pybind
+
+    def configure(self):
+        """The configure method."""
+        # self.version = envvars.get('XMS_VERSION', '99.99.99')
+
+        # Raise ConanExceptions for Unsupported Versions
+        s_os = self.settings.os
+        s_compiler = self.settings.compiler
+        s_compiler_version = self.settings.compiler.version
+
+        if s_compiler == "apple-clang" and s_os == 'Linux':
+            raise ConanException("Clang on Linux is not supported.")
+
+        if s_compiler == "gcc" and float(s_compiler_version.value) < 5.0:
+            raise ConanException("GCC < 5.0 is not supported.")
+
+        if s_compiler == "apple-clang" and s_os == 'Macos' and float(s_compiler_version.value) < 9.0:
+            raise ConanException("Clang > 9.0 is required for Mac.")
+
+        for dependency in self.xms_dependencies:
+            dep_name, _, _ = dependency.split('/')
+            self.options[dep_name].pybind = self.options.pybind
+            self.options[dep_name].testing = self.options.testing
+
+    def layout(self):
+        """The layout method."""
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+
+        # CxxTest doesn't play nice with PyBind. Also, it would be nice to not
+        # have tests in release code. Thus, if we want to run tests, we will
+        # build a test version (without python), run the tests, and then (on
+        # success) rebuild the library without tests.
+        # breakpoint()
+        tc.variables["IS_PYTHON_BUILD"] = self.options.pybind
+        tc.variables["BUILD_TESTING"] = self.options.testing
+        tc.variables["XMS_TEST_PATH"] = "test_files"
+
+        # Version Info
+        tc.variables["XMS_VERSION"] = '{}'.format(self.version)
+        tc.variables["PYTHON_TARGET_VERSION"] = os.environ.get("PYTHON_TARGET_VERSION", "3.10")
+
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
+
+    def build(self):
+        """The build method for the conan class."""
+        cmake = CMake(self)
+
+        variables = {}
+        variables["IS_PYTHON_BUILD"] = self.options.pybind
+        variables["BUILD_TESTING"] = self.options.testing
+        variables["XMS_TEST_PATH"] = "test_files"
+
+        # Version Info
+        variables["XMS_VERSION"] = '{}'.format(self.version)
+        variables["PYTHON_TARGET_VERSION"] = os.environ.get("PYTHON_TARGET_VERSION", "3.10")
+
+
+        cmake.configure(variables=variables)
+        cmake.build()
+        cmake.install()
+
+        # Run the tests if it is testing configuration.
+        if self.options.testing:
+            self.run_cxx_tests(cmake)
+
+        # If this build is python run the python tests.
+        elif self.options.pybind:
+            self.run_python_tests_and_upload()
+
+    def package(self):
+        """The package method of the conan class."""
+        cmake = CMake(self)
+        cmake.install()
+        copy(self, "license", src=os.path.join(self.source_folder), dst=os.path.join(self.package_folder, "licenses"), ignore_case=True, keep_path=False)
+
+    def package_info(self):
+        """The package_info method of the conan class."""
+        if self.options.pybind:
+            self.env_info.PYTHONPATH.append(os.path.join(self.package_folder, "_package"))
+        if self.settings.build_type == 'Debug':
+            self.cpp_info.libs = [f'{self.name}lib_d']
+        else:
+            self.cpp_info.libs = [f'{self.name}lib']
+
+    def run_cxx_tests(self, cmake):
+        """A function to run the cxx_tests."""
+        try:
+            cmake.test()
+        except ConanException:
+            raise
+        finally:
+            if os.path.isfile("TEST-cxxtest.xml"):
+                with open("TEST-cxxtest.xml", "r") as f:
+                    for line in f.readlines():
+                        no_newline = line.strip('\n')
+                        self.output.info(no_newline)
+
+    def run_python_tests_and_upload(self):
+        """A method to run the python tests, and upload if this is a release."""
+        with tools.pythonpath(self):
+
+            # Install required packages for python testing.
+            packages_to_install = ['"numpy<2.0"', 'wheel']
+            if not self.settings.os == "Macos":
+                self.run(f'pip install --user {" ".join(packages_to_install)}')
+            else:
+                self.run(f'pip install {" ".join(packages_to_install)}')
+
+            # Run python tests.
+            path_to_python_tests = os.path.join(self.source_folder, '_package', 'tests')
+            self.run(f'python -m unittest discover -v -p *_pyt.py -s {path_to_python_tests}',
+                     cwd=os.path.join(self.package_folder, "_package"))
+
+            # Create and upload wheel to aquapi if release and windows
+            # We are uploading to aquapi here instead of pypi because pypi doesn't accept
+            # the type of package 'linux_x86_64 that we want to upload. They only accept
+            # manylinux1 as the plat-tag
+            is_release = os.environ.get("RELEASE_PYTHON", 'False') == 'True'
+            is_mac_os = self.settings.os == 'Macos'
+            is_gcc_7 = self.settings.os == "Linux" and float(self.settings.compiler.version.value) == 7.0
+            is_windows_md = (self.settings.os == "Windows" and str(self.settings.compiler.runtime) == "MD")
+            if is_release and (is_mac_os or is_gcc_7 or is_windows_md):
+                self.upload_python_package()
+
+    def upload_python_package(self):
+        """Upload the python package to AQUAPI_URL."""
+        devpi_url = os.environ.get("AQUAPI_URL", 'NO_URL')
+        devpi_username = os.environ.get("AQUAPI_USERNAME", 'NO_USERNAME')
+        devpi_password = os.environ.get("AQUAPI_PASSWORD", 'NO_PASSWORD')
+        self.run('devpi use {}'.format(devpi_url))
+        self.run('devpi login {} --password {}'.format(devpi_username, devpi_password))
+        self.run('python setup.py bdist_wheel --dist-dir {}'.format(os.path.join(self.build_folder, "dist")), cwd=os.path.join(self.package_folder, "_package"))
+        self.run('devpi upload --from-dir {}'.format(os.path.join(self.build_folder, "dist")), cwd=".")
+
+    def export_sources(self):
+        """Specify sources to be exported."""
+        copy(self, '*', src=os.path.join(self.recipe_folder, f'{self.name}'), dst=os.path.join(self.export_sources_folder, f'{self.name}'))
+        copy(self, '*', src=os.path.join(self.recipe_folder, '_package'), dst=os.path.join(self.export_sources_folder, '_package'))
+        copy(self, 'CMakeLists.txt', src=self.recipe_folder, dst=self.export_sources_folder)
+
+        for item in self.extra_export_sources:
+            if os.path.isdir(item):
+                copy(self, '*', src=os.path.join(self.recipe_folder, f'{item}'), dst=os.path.join(self.export_sources_folder, f'{item}'))
+            else:
+                copy(self, f'{item}', src=self.recipe_folder, dst=self.export_sources_folder)
+
+    def export(self):
+        """Specify files to be exported."""
+        self.output.info("Exporting files...")
+        copy(self, 'LICENSE', src=self.recipe_folder, dst=self.export_folder)
+        for item in self.extra_exports:
+            if os.path.isdir(item):
+                copy(self, '*', src=os.path.join(self.recipe_folder, f'{item}'), dst=os.path.join(self.export_folder, f'{item}'))
+            else:
+                copy(self, f'{item}', src=self.recipe_folder, dst=self.export_folder)
